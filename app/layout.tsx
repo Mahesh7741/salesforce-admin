c72dab48ff426b96e51e3be56ee85346f8c8b0a8
Rajjadhav423 <2021bit057@sggs.ac.in>
2025-07-06 18:05:07 +0530

    hyderation solved

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,16 +44,20 @@ export const metadata: Metadata = {
   description: "Created with v0",
   generator: "v0.dev",
 }
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
